8960b698ec03911ddb19420021c0e8ab9a94cf7c
Kimberly Brown <kbrown1@unc.edu>
2023-04-07 19:06:00 -0400

    edit longitude/latitude

--- cli.js
+++ cli.js
@@ -29,7 +29,7 @@ if (args.n) {
 } else if (args.s) {
     latitude = -args.s;
 } else {
-  console.log("Latitude not in range");
+  console.log("Latitude must be in range");
   process.exit(0);
 }
 
@@ -38,7 +38,7 @@ if (args.e) {
 } else if (args.w ) {
     longitude = -args.w;
 } else {
-  console.log("Longitude not in range");
+  console.log("Longitude must be in range");
   process.exit(0);
 }
 
